6f7327bffda90b8a617d94fc9f6571a3c1caa9a9
Sarthak Tyagi <sarthak98tyagi@gmail.com>
2020-07-08 12:12:29 +0530

    CSS Configured

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.module.css';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className={classes.App}>
     </div>
   );
 }
