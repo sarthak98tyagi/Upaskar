f0781231287f7901dd7003285946fedc5bc7b6b1
Sarthak Tyagi <sarthak98tyagi@gmail.com>
2020-07-18 17:51:12 +0530

    Updates

---
+++ precache-manifest.26744ced25d07294e7ea172abc1260f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9941d82b372a0ec5e1c5f1db2f9c1fea",
+    "url": "/Upaskar/index.html"
+  },
+  {
+    "revision": "8e87d6b9a667545a696d",
+    "url": "/Upaskar/static/css/main.e5847781.chunk.css"
+  },
+  {
+    "revision": "bd70977d83d64e162003",
+    "url": "/Upaskar/static/js/2.82ca2c44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Upaskar/static/js/2.82ca2c44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e87d6b9a667545a696d",
+    "url": "/Upaskar/static/js/main.9e629392.chunk.js"
+  },
+  {
+    "revision": "4bd6577c82e587a25f55",
+    "url": "/Upaskar/static/js/runtime-main.257368f0.js"
+  }
+]);
